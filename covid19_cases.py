d8b44eae9992241bc20de4e3eb6db0c3c88b0558
Jonathan Cai <jonathan.y.cai@outlook.com>
2025-03-06 00:59:02 -0800

    Create Facebook prophet model to make predictions for the next 30 days

--- covid19_cases.py
+++ covid19_cases.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+np.float = float
 import matplotlib.pyplot as plt
 import plotly.express as px
 
@@ -108,4 +109,40 @@ df.Deaths.plot(title="Daily Covid19 Deaths in World",marker=".",figsize=(10,5),l
 df.Deaths.rolling(window=5).mean().plot(figsize=(10,5),label="MA5")
 plt.ylabel("Deaths")
 plt.legend()
+plt.show()
+
+class Fbprophet(object):
+    def fit(self,data):
+        
+        self.data  = data
+        self.model = Prophet(weekly_seasonality=True,daily_seasonality=False,yearly_seasonality=False)
+        self.model.fit(self.data)
+    
+    def forecast(self,periods,freq):
+        
+        self.future = self.model.make_future_dataframe(periods=periods,freq=freq)
+        self.df_forecast = self.model.predict(self.future)
+        
+    def plot(self,xlabel="Years",ylabel="Values"):
+        
+        self.model.plot(self.df_forecast,xlabel=xlabel,ylabel=ylabel,figsize=(9,4))
+        self.model.plot_components(self.df_forecast,figsize=(9,6))
+        
+    def R2(self):
+        return r2_score(self.data.y, self.df_forecast.yhat[:len(df)])
+        
+df_fb  = pd.DataFrame({"ds":[],"y":[]})
+df_fb["ds"] = pd.to_datetime(df.index)
+df_fb["y"]  = df.iloc[:,0].values
+
+model = Fbprophet()
+model.fit(df_fb)
+model.forecast(30,"D")
+model.R2()
+
+forecast = model.df_forecast[["ds","yhat_lower","yhat_upper","yhat"]].tail(30).reset_index().set_index("ds").drop("index",axis=1)
+forecast["yhat"].plot(marker=".",figsize=(10,5))
+plt.fill_between(x=forecast.index, y1=forecast["yhat_lower"], y2=forecast["yhat_upper"],color="gray")
+plt.legend(["forecast","Bound"],loc="upper left")
+plt.title("Forecasting of Next 30 Days Cases")
 plt.show()
